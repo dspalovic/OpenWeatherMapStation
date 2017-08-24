0548a7b546ede0c3efd2a916357b367e5a8e2987
Danilo Spalovic <dspalovic@gmail.com>
2017-08-24 08:44:10 +0200

    added libraries and changed arduino file for BMP180, LCD

--- arduino/owms/owms.ino
+++ arduino/owms/owms.ino
@@ -1,3 +1,14 @@
+// LCD
+#include <LiquidCrystal.h>    // include LiquidCrystal library
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);    //initialize LCD
+
+// BMP180
+#include <SFE_BMP180.h>     // include BMP180 library
+#include <Wire.h>     // include wire library
+SFE_BMP180 pressure;      // create object pressure
+#define ALTITUDE 485.0      // altitutde of mesuring point
+
+// DHT22
 #include "DHT.h"      // include library for DHT sensors
 #define DHTPIN 8      // digital pin DHT is connected to
 #define DHTTYPE DHT22     // type of DHT sensor
@@ -6,21 +17,180 @@ DHT dht(DHTPIN, DHTTYPE);     // initialize DHT sensor
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);     // serial for debuging
-  dht.begin();
+  lcd.begin(16, 2);     // define LCD 16x2 and start
+  dht.begin();      // start DHT22
+  // start BMP180
+  if (pressure.begin()){
+    Serial.println("BMP180 init success");
+    lcd.clear();
+    lcd.print("BMP180 init success");
+  }
+  else
+  {
+    // Oops, something went wrong, this is usually a connection problem,
+    // see the comments at the top of this sketch for the proper connections.
+
+    Serial.println("BMP180 init fail\n\n");
+    lcd.clear();
+    lcd.print("BMP180 init faill");
+    while(1); // Pause forever.
+  }
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  float h = dht.readHumidity();     // read humidity
-  float t = dht.readTemperature();      // read temperature as Celsius
-  if (isnan(h) || isnan(t)) {
+    char status;
+  double T,P,p0,a;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+  
+  // If you want sea-level-compensated pressure, as used in weather reports,
+  // you will need to know the altitude at which your measurements are taken.
+  // We're using a constant called ALTITUDE in this sketch:
+  
+  Serial.println();
+  Serial.print("provided altitude: ");
+  Serial.print(ALTITUDE,0);
+  Serial.print(" meters, ");
+  Serial.print(ALTITUDE*3.28084,0);
+  Serial.println(" feet");
+  
+  // If you want to measure altitude, and not pressure, you will instead need
+  // to provide a known baseline pressure. This is shown at the end of the sketch.
+
+  // You must first get a temperature measurement to perform a pressure reading.
+  
+  // Start a temperature measurement:
+  // If request is successful, the number of ms to wait is returned.
+  // If request is unsuccessful, 0 is returned.
+
+  status = pressure.startTemperature();
+  if (status != 0)
+  {
+    // Wait for the measurement to complete:
+    delay(status);
+
+    // Retrieve the completed temperature measurement:
+    // Note that the measurement is stored in the variable T.
+    // Function returns 1 if successful, 0 if failure.
+
+    status = pressure.getTemperature(T);
+    if (status != 0)
+    {
+      // Print out the measurement:
+      Serial.print("temperature: ");
+      Serial.print(T,2);
+      Serial.print(" deg C, ");
+      Serial.print((9.0/5.0)*T+32.0,2);
+      Serial.println(" deg F");
+      lcd.print("T");
+      lcd.print(T,2);
+      lcd.print("C Rh");
+      lcd.print(h);
+      lcd.print("%");
+      
+      // Start a pressure measurement:
+      // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
+      // If request is successful, the number of ms to wait is returned.
+      // If request is unsuccessful, 0 is returned.
+
+      status = pressure.startPressure(3);
+      if (status != 0)
+      {
+        // Wait for the measurement to complete:
+        delay(status);
+
+        // Retrieve the completed pressure measurement:
+        // Note that the measurement is stored in the variable P.
+        // Note also that the function requires the previous temperature measurement (T).
+        // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
+        // Function returns 1 if successful, 0 if failure.
+
+        status = pressure.getPressure(P,T);
+        if (status != 0)
+        {
+          // Print out the measurement:
+          Serial.print("absolute pressure: ");
+          Serial.print(P,2);
+          Serial.print(" mb, ");
+          Serial.print(P*0.0295333727,2);
+          Serial.println(" inHg");
+          lcd.setCursor(0, 1);
+          lcd.print("P");
+          lcd.print(P,2);
+          lcd.print("mb ");
+          lcd.print(hic);
+          lcd.print("C");
+
+          // The pressure sensor returns abolute pressure, which varies with altitude.
+          // To remove the effects of altitude, use the sealevel function and your current altitude.
+          // This number is commonly used in weather reports.
+          // Parameters: P = absolute pressure in mb, ALTITUDE = current altitude in m.
+          // Result: p0 = sea-level compensated pressure in mb
+
+          p0 = pressure.sealevel(P,ALTITUDE); // we're at 1655 meters (Boulder, CO)
+          Serial.print("relative (sea-level) pressure: ");
+          Serial.print(p0,2);
+          Serial.print(" mb, ");
+          Serial.print(p0*0.0295333727,2);
+          Serial.println(" inHg");
+
+          // On the other hand, if you want to determine your altitude from the pressure reading,
+          // use the altitude function along with a baseline pressure (sea-level or other).
+          // Parameters: P = absolute pressure in mb, p0 = baseline pressure in mb.
+          // Result: a = altitude in m.
+
+          a = pressure.altitude(P,p0);
+          Serial.print("computed altitude: ");
+          Serial.print(a,0);
+          Serial.print(" meters, ");
+          Serial.print(a*3.28084,0);
+          Serial.println(" feet");
+        }
+        else Serial.println("error retrieving pressure measurement\n");
+      }
+      else Serial.println("error starting pressure measurement\n");
     }
-  float hic = dht.computeHeatIndex(t, h, false);      // compute heat index in Celsius
-  Serial.println(h);
-  Serial.println(t);
-  Serial.println(hic);
+    else Serial.println("error retrieving temperature measurement\n");
+  }
+  else Serial.println("error starting temperature measurement\n");
+
+  delay(5000);  // Pause for 5 seconds.
 
 }
